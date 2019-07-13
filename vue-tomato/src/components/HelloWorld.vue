02072031896ecb9635d5b1fa00592c8dd98a9bfc
kelly6414143 <kelly6414143@gmail.com>
2019-07-13 17:06:48 +0800

    init

---
+++ vue-tomato/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$vue-color: #42b983;
+h1 {
+  color: $vue-color;
+}
+</style>
